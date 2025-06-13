c6872cf2dea7424cb185b482bf61390575f8085a
Habert Kungu <habert231@gmail.com>
2025-06-13 17:50:30 +0300

    adding structy linked list

---
+++ structy/linkedlist/insertNode.js
@@ -0,0 +1,35 @@
+const insertNode = (head, value, index) => {
+  let current = head;
+  let count = 0;
+  if (index === 0) {
+    const newHead = new Node(value);
+    newHead.next = head;
+    return newHead;
+  }
+  while (current !== null) {
+    if (index - 1 === count) {
+      const temp = current.next;
+      current.next = new Node(value);
+      current.next.next = temp;
+
+      break;
+    }
+    value = current;
+    count++;
+    current = current.next;
+  }
+  return head;
+};
+const a = new Node("a");
+const b = new Node("b");
+const c = new Node("c");
+const d = new Node("d");
+
+a.next = b;
+b.next = c;
+c.next = d;
+
+// a -> b -> c -> d
+
+insertNode(a, "x", 2);
+// a -> b -> x -> c -> d
