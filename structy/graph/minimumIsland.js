1ed2e3f1c39b582b27ac332d469329ab7e9735d3
Habert Kungu <habert231@gmail.com>
2025-07-30 20:57:36 +0300

    Update: Adding graph algorithms

--- structy/graph/minimumIsland.js
+++ structy/graph/minimumIsland.js
@@ -1,5 +1,35 @@
 const minimumIsland = (grid) => {
-  // todo
+  const visited = new Set();
+  let minSize = -Infinity;
+  for (let r = 0; r < grid.length; r++) {
+    for (let c = 0; c < grid[0].length; c++) {
+      const size = exploreSize(grid, r, c, visited);
+      if (size > 0 && size < minSize) {
+        minSize = size;
+      }
+    }
+  }
+  return minSize;
+};
+
+const exploreSize = (grid, r, c, visited) => {
+  let count = 0;
+  const rowInbound = 0 <= r && r < grid.length;
+  const colInbound = 0 <= c && c < grid[0].length;
+  const position = r + "," + c;
+
+  if (!rowInbound || !colInbound) return 0;
+  if (grid[r][c] === "W") return 0;
+  if (visited.has(position)) return 0;
+  visited.add(position);
+
+  let size = 1;
+  size += exploreSize(grid, r - 1, c, visited);
+  size += exploreSize(grid, r + 1, c, visited);
+  size += exploreSize(grid, r, c - 1, visited);
+  size += exploreSize(grid, r, c + 1, visited);
+
+  return size;
 };
 const grid = [
   ["W", "L", "W", "W", "W"],
